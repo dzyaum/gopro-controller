3656be602b1bbbab335a7b545b65ec9f72060100
Brian <brojoedro@icloud.com>
2026-05-17 20:49:29 -0400

    Add GoPro connection test script

---
+++ test_connect.py
@@ -0,0 +1,6 @@
+﻿from goprocam import GoProCamera, constants
+
+print("Connecting to GoPro...")
+cam = GoProCamera.GoPro(constants.gpcontrol)
+print("Connected!")
+print(cam.getStatus(constants.Status.Status, constants.Status.STATUS.Model))
